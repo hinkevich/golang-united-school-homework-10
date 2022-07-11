c3985460be688a759f051f6c5ee50a2c85bf2f57
hinkevich <hinkevich@gmail.com>
2022-07-11 17:16:32 +0300

    add changes

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -17,14 +18,22 @@ Feel free to drop gorilla.mux if you want and use any other solution available.
 main function reads host/port from env just for an example, flavor it following your taste
 */
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	//w.Write([]byte("Status: 500"))
+	w.Header().Set("Trailer", "AtEnd1, AtEnd2")
+	w.Header().Add("Trailer", "AtEnd3")
 
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusInternalServerError)
+	w.WriteHeader(200)
+	w.Header().Set("AtEnd1", "value 1")
+	io.WriteString(w, "Status:"+http.StatusText(http.StatusInternalServerError))
+	w.Header().Set("AtEnd2", "value 2")
+	w.Header().Set("AtEnd3", "value 3") // These will appear as trailers.
 	return
 }
 func handlerTwo(w http.ResponseWriter, r *http.Request) {
 	name := mux.Vars(r)["PARAM"]
-	w.Write([]byte("Hello, " + name + "!"))
+	w.WriteHeader(200)
+	w.Write([]byte("body: Hello, " + name + "!"))
 
 	return
 }
